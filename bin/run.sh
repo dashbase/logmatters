23bddae3988fb457bb688ef5aee076c93d87b87c
Chao <oleooo@126.com>
2018-12-07 23:17:53 +0800

    add nohup logpath

--- bin/run.sh
+++ bin/run.sh
@@ -16,9 +16,9 @@ epoch=`date +%s`
 for i in `seq 1 $1`
 do
     if [ x"$2" = x ]; then
-        nohup java  -Dprocessname=dashbase-log -Didentifier=$epoch -Dtotal=$1 ${JAVA_OPTS} ${HEAP_OPTS} ${GC_OPTS} ${JMX_OPTS} ${JAVA_DEBUG} -Ddw.logging.appenders[0].currentLogFilename=${logs}/dashbase-$epoch-$i.log -Ddw.logging.appenders[0].archivedLogFilenamePattern=${logs}/dashbase-$epoch-$i.log.%i -jar ${dist}/logmatters-0.0.1-SNAPSHOT.jar server ${conf}/config.yml &
+        nohup java  -Dprocessname=dashbase-log -Didentifier=$epoch -Dtotal=$1 ${JAVA_OPTS} ${HEAP_OPTS} ${GC_OPTS} ${JMX_OPTS} ${JAVA_DEBUG} -Ddw.logging.appenders[0].currentLogFilename=${logs}/dashbase-$epoch-$i.log -Ddw.logging.appenders[0].archivedLogFilenamePattern=${logs}/dashbase-$epoch-$i.log.%i -jar ${dist}/logmatters-0.0.1-SNAPSHOT.jar server ${conf}/config.yml > /tmp/logmatters.log &
     else
-        nohup java -Dprocessname=dashbase-log -Didentifier=$epoch -Ddw.throttleNPerSec=$2 -Dtotal=$1 ${JAVA_OPTS} ${HEAP_OPTS} ${GC_OPTS} ${JMX_OPTS} ${JAVA_DEBUG} -Ddw.logging.appenders[0].currentLogFilename=${logs}/dashbase-$epoch-$i.log -Ddw.logging.appenders[0].archivedLogFilenamePattern=${logs}/dashbase-$epoch-$i.log.%i -jar ${dist}/logmatters-0.0.1-SNAPSHOT.jar server ${conf}/config.yml &
+        nohup java -Dprocessname=dashbase-log -Didentifier=$epoch -Ddw.throttleNPerSec=$2 -Dtotal=$1 ${JAVA_OPTS} ${HEAP_OPTS} ${GC_OPTS} ${JMX_OPTS} ${JAVA_DEBUG} -Ddw.logging.appenders[0].currentLogFilename=${logs}/dashbase-$epoch-$i.log -Ddw.logging.appenders[0].archivedLogFilenamePattern=${logs}/dashbase-$epoch-$i.log.%i -jar ${dist}/logmatters-0.0.1-SNAPSHOT.jar server ${conf}/config.yml > /tmp/logmatters.log &
     fi
     echo "Started the process $i with Log name: /logs/dashbase-$epoch-$i.log..."
 done

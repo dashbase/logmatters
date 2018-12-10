d24929a92cd5f13a9b8f0b37bced99234efc0b2e
John Wang <john.wang@gmail.com>
2018-12-09 17:15:48 -0800

    hook up metrics and refactor (#3)
    * hook up metrics and refactor
    
    * metrics
    
    * use random port by default
    
    * cleanup

--- src/main/java/io/dashbase/logmatters/sample/LogmattersApplication.java
+++ src/main/java/io/dashbase/logmatters/sample/LogmattersApplication.java
@@ -1,11 +1,18 @@
 package io.dashbase.logmatters.sample;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Meter;
 import io.dropwizard.Application;
+import io.dropwizard.lifecycle.Managed;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.dhatim.dropwizard.prometheus.PrometheusBundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import static io.dashbase.logmatters.sample.TestLog.logIt;
 
@@ -15,19 +22,48 @@ public class LogmattersApplication extends Application<LogmattersConfiguration> 
 
     private Random rand = new Random();
     private volatile boolean isStopped = false;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public static void main(String[] args) throws Exception {
         new LogmattersApplication().run(args);
     }
 
     @Override
+    public void initialize(Bootstrap<LogmattersConfiguration> bootstrap) {
+        bootstrap.addBundle(new PrometheusBundle());
+    }
+
+    @Override
     public void run(LogmattersConfiguration conf, Environment environment) throws Exception {
+        Meter eventMeter = environment.metrics().meter("log_event_count");
         long sleepDuration = (long)(1000.0/(double)conf.throttleNPerSec);
-        while(!isStopped) {
-            logIt(logger);
-            if (sleepDuration >=0) {
-                Thread.sleep(sleepDuration);
+        environment.lifecycle().manage(new Managed() {
+            @Override
+            public void start() throws Exception {
+                scheduler.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        while(!isStopped) {
+                            logIt(logger);
+                            eventMeter.mark();
+                            if (sleepDuration >=0) {
+                                try {
+                                    Thread.sleep(sleepDuration);
+                                } catch (InterruptedException ie) {
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                });
             }
-        }
+
+            @Override
+            public void stop() throws Exception {
+                isStopped = true;
+                scheduler.shutdown();
+            }
+        });
+        logger.info("logmatters: " + conf.name + " started");
     }
 }
--- src/main/java/io/dashbase/logmatters/sample/LogmattersConfiguration.java
+++ src/main/java/io/dashbase/logmatters/sample/LogmattersConfiguration.java
@@ -4,4 +4,5 @@ import io.dropwizard.Configuration;
 
 public class LogmattersConfiguration extends Configuration {
     public int throttleNPerSec = 1;
+    public String name = "logmatters";
 }

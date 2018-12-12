39250c625c6ac98e6d39a29068a91934316ef960
Joway Wang <joway.w@gmail.com>
2018-12-12 19:18:34 +0800

    add bin/entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 logs=${LOG_PATH:-/logs}
+conf=${CONFIG_PATH:-./conf/config.yml}
+loop=$1
 
-java  -Dprocessname=dashbase-log -Didentifier=$epoch \
-      -Dtotal=$1 ${JAVA_OPTS} ${HEAP_OPTS} ${GC_OPTS} ${JMX_OPTS} ${JAVA_DEBUG} \
-      -Ddw.logging.appenders[0].currentLogFilename=${LOG_PA}/dashbase-$epoch-$i.log \
-      -Ddw.logging.appenders[0].archivedLogFilenamePattern=${logs}/dashbase-$epoch-$i.log.%i \
-      -jar ${dist}/logmatters-*.jar server ${conf}/config.yml
+for i in `seq 1 $loop`
+do
+epoch=`date +%s`
+nohup java  -Dprocessname=dashbase-log -Didentifier=$epoch \
+      -Dtotal=$loop ${JAVA_OPTS} ${HEAP_OPTS} ${GC_OPTS} ${JMX_OPTS} ${JAVA_DEBUG} \
+      -Ddw.logging.appenders[0].currentLogFilename=${LOG_PA}/dashbase-$epoch.log \
+      -Ddw.logging.appenders[0].archivedLogFilenamePattern=${logs}/dashbase-$epoch.log.%i \
+      -jar ./target/logmatters-*.jar server ${conf} > /tmp/logmatters.log &
+done
+
+# sleep
+while true; do; sleep 100; done
